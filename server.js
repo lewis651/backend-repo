7864f425a9cf96619d69f3f99b123a72e604673c
lewis651 <nchonganyilewis1@gmail.com>
2026-01-19 01:36:35 +0100

    upade2

--- server.js
+++ server.js
@@ -11,14 +11,7 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors({
-  origin: [
-    "http://localhost:5173",
-    "http://localhost:5174",
-    process.env.CLIENT_URL
-  ].filter(Boolean),
-  credentials: true
-}));
+app.use(cors());
 app.use(express.json());
 app.use('/uploads', express.static('uploads'));
 
